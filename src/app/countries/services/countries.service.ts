8e7698744e7687b02241eaeabfb8bd6315c506ac
CodeJairo <jairo.herrera729@pascualbravo.edu.co>
2024-05-05 18:31:42 -0500

    chore: Update country-page.component.ts to implement lazy loading for country data

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({
@@ -10,6 +10,14 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) {}
+
+  searchCountryByCode(code: string): Observable<Country | null> {
+    return this.http.get<Country[]>(`${this.apiUrl}/alpha/${code}`).pipe(
+      map((countries) => (countries.length > 0 ? countries[0] : null)),
+
+      catchError(() => of(null))
+    );
+  }
 
   searchCapital(query: string): Observable<Country[]> {
     return this.http
