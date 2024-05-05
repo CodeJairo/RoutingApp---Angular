c02d9f5ae8547b4607075a22309fa017ce776251
CodeJairo <jairo.herrera729@pascualbravo.edu.co>
2024-05-05 14:36:06 -0500

    chore: Add HttpClientModule and SharedModule to app.module.ts, update index.html with Bootstrap CSS, and update app-routing.module.ts with new routes

---
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'countries-country-page',
+  templateUrl: './country-page.component.html',
+
+})
+export class CountryPageComponent {
+
+}
