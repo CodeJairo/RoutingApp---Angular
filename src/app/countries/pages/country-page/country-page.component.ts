09bd756628c11ca761ecc8b4358a15a1b20d79ad
CodeJairo <jairo.herrera729@pascualbravo.edu.co>
2024-05-05 19:18:14 -0500

    chore: Update country-page.component.html and country-page.component.ts to display country details and implement lazy loading

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ChildActivationEnd, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-country-page',
   templateUrl: './country-page.component.html',
 })
 export class CountryPageComponent implements OnInit {
+  public country?: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private countriesService: CountriesService,
@@ -20,10 +23,10 @@ export class CountryPageComponent implements OnInit {
         switchMap(({ id }) => this.countriesService.searchCountryByCode(id))
       )
       .subscribe((country) => {
-        if (!country) {
-          return this.router.navigateByUrl('');
-        }
-        return;
+        if (!country) return this.router.navigateByUrl('');
+
+        return (this.country = country);
+
       });
   }
 }
