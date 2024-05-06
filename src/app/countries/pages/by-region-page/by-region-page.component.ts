af880f3846eb085d49bb8c0e9795568b943f0e27
CodeJairo <jairo.herrera729@pascualbravo.edu.co>
2024-05-06 16:21:18 -0500

    chore: refactored

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
+
+type Region = 'Africa' | 'America' | 'Asia' | 'Europe' | 'Oceania';
 
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion: Region = 'Africa';
+
+  ngOnInit(): void {
+    if (this.regions.length > 0) {
+      this.searchByRegion(this.regions[0]);
+    }
+  }
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(query: string): void {
+  searchByRegion(query: Region): void {
+    this.selectedRegion = query;
     this.isLoading = true;
     this.countriesService.searchRegion(query).subscribe((countries) => {
       this.countries = countries;
